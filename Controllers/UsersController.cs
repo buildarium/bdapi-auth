ffd90c1ce5f46fc398171ca341871b9f3911b505
Buck Tower <buck@bucktower.net>
2019-05-02 21:04:45 -0500

    Working query

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bdapi_auth.Models;
+using bdapi_auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bdapi_auth.Controllers
+{
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _userService.Users.AsEnumerable();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using bdapi_auth.Models;
+using Microsoft.EntityFrameworkCore;
+using bdapi_auth.Services;
 
 namespace bdapi_auth
 {
@@ -25,7 +28,9 @@ namespace bdapi_auth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<UserService>(options =>
+               options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,10 +43,10 @@ namespace bdapi_auth
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
